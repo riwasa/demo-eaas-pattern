d8dbca137d168d12e8c1f7bac874fac2fc2cf711
Richard Iwasa <riwasa@microsoft.com>
2025-06-09 09:15:30 -0400

    Change cost logic

---
+++ Calculate-LifetimeCost2.ps1
@@ -0,0 +1,41 @@
+param(
+  [Parameter(Mandatory = $true, Position = 0)]
+  [string]$lobFilePath
+)
+
+param(
+  [Parameter(Mandatory = $true, Position = 1)]
+  [string]$eaasStampFileRoot
+)
+
+Write-Host "Reading LOB parameters from file: $lobFilePath"
+$json = Get-Content -Path $lobFilePath -Raw | ConvertFrom-Json
+
+$eaasStampSize = $json.parameters.eaasStampSize.value
+Write-Host "EaaS stamp size: $eaasStampSize"
+
+$eaasEndDate = $json.parameters.eaasEndDate.value
+Write-Host "EaaS end date: $eaasEndDate"
+
+$eaasStampFilePath = "$eaasStampFileRoot/$($eaasStampSize.ToLower())-param.json"
+Write-Host "Reading EaaS stamp file paramaters from file: $eaasStampFilePath"
+$eaasStampJson = Get-Content -Path $eaasStampFilePath -Raw | ConvertFrom-Json
+
+$dailyRunCost = $eaasStampJson.parameters.dailyRunCost.value
+Write-Host "Daily run cost: $dailyRunCost"
+
+Write-Host "Calculating lifetime cost based on daily run cost: $dailyRunCost and end date: $eaasEndDate"
+
+# Convert the daily run cost to a decimal
+$dailyRunCostDecimal = [decimal]$dailyRunCost
+# Convert the end date to a DateTime object
+$endDate = [datetime]$eaasEndDate
+# Get the current date
+$currentDate = Get-Date
+# Calculate the number of days until the end date
+$runtimeDays = ($endDate - $currentDate).Days
+# Calculate the lifetime cost
+$lifetimeCost = $dailyRunCostDecimal * $runtimeDays
+
+Write-Host "Calculated lifetime cost: $lifetimeCost"
+Write-Output $lifetimeCost
